89de9598d8c2e642a6dc506f179dcea19abc3ab6
Ted Moke <iam@tedmoke.com>
2020-04-12 16:55:28 -0400

    Deletes old files

--- oldStructure/src/js/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    TRANSITION_DURATION: 0.35,
-    colors: ['#F48907', '#D95D39', '#575366', '#0E1428'],
-    colorsGray: ['#888', '#666', '#444', '#222'],
-    DESCRIPTION_CYCLE_INTERVAL_SHORT: 3500,
-    DESCRIPTION_CYCLE_INTERVAL_LONG: 7500
-};
-
-export default config;
